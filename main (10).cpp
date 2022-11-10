f33d1c26bb3dd5b1c1f3bd7e34a3ad524aae123c
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-10 21:55:42 +0530

    Add files via upload

---
+++ main (10).cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class Node{
+public:
+     int data;
+      Node* left;
+      Node* right;
+      Node(int d){
+        this->data=d;
+        this->left=NULL;
+          this->right=NULL;
+      }
+};
+Node* InsertIntoBST(Node*root, int d){
+  //base case
+  if(!root){
+    root=new Node(d);
+    return root;
+  }
+  if(d> root->data){
+    root->right = InsertIntoBST(root->right,d);
+  }
+  else{
+     root->left = InsertIntoBST(root->left,d);
+  }
+  return root;
+}
+
+void inorder(Node*root){
+  if(root==NULL)
+    return;
+  inorder(root->left);
+  cout<<root->data<<" ";
+  inorder(root->right);
+}
+void TakeInput(Node* &root){
+  int data;
+  cin>>data;
+
+  while(data!=-1){
+    root=InsertIntoBST(root, data);
+    cin>>data;
+  }
+}
+int main() {
+  Node* root=NULL;
+  cout<<"Enter data into BST"<<endl;
+  TakeInput(root);
+  cout<<"printing BST"<<endl;
+  inorder(root);
+}
