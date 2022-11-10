3f44018cded38f2f1a7a4593a41dc94cf29903ed
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-10 22:33:01 +0530

    Update main (11).cpp

--- main (11).cpp
+++ main (11).cpp
@@ -1,15 +1,13 @@
 #include <iostream>
+//Time complexity: O(Height of tree) or O(N) (in case of a skewed tree)
 bool search(Node* root, int x) {
     //base case
     if(root==NULL)
        return false;
-     if(root->data == x)
+    if(root->data == x)
         return true;
     if(root->data > x)
-    return search(root->left, x);
+       return search(root->left, x);
     else
-      return search(root->right, x);
-}
-int main() {
-  std::cout << "Hello World!\n";
+       return search(root->right, x);
 }
