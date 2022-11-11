25b40b6282da49d8031760dbad932ddafbd54b15
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-11 09:56:37 +0530

    Add files via upload

---
+++ main (12).cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+int MinVal(TreeNode* root){  //finds the min value in the tree
+     TreeNode* temp=root;
+    while(temp->left)
+        temp = temp->left;
+    return temp->val;
+ }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        //base case
+        if(!root)
+            return NULL;
+
+        if(root->val == key){
+             //0 child
+             if(!root->left && !root->right){
+                 delete root;
+                 return NULL;
+             }
+            //1 child
+            if(root->left == NULL && root->right != NULL){
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+            if(root->left != NULL && root->right == NULL){
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+            //2 children
+            if(root->left && root->right){   // Find the min value in the right subtree and replace the root's value by it. Then delete the node which contained min
+                int min = MinVal(root->right);
+                root->val = min;
+                root->right = deleteNode(root->right, min);
+                return root;
+            }
+        }
+
+        else if(root->val > key)  //visit the left subtree
+           root->left = deleteNode(root->left, key);
+        
+        else  //visit the right subtree
+            root->right = deleteNode(root->right, key);
+        
+         return root;
+    }
+};
+int main() {
+  std::cout << "Hello World!\n";
+}
