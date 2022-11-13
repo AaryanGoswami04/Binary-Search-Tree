819821cc1a51b61416835197a2641ec1205dd76b
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-13 11:21:29 +0530

    Add files via upload

---
+++ main (10).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int solve(TreeNode* root,int &i, int k){
+    //base case
+    if(!root)
+        return -1;
+    //Left subtree
+    int left = solve(root->left, i, k);
+
+    if(left != -1) //Kth smallest element has been found already so return it to parent
+       return left;
+    
+    //Reached root
+    i++;
+    if(i==k)
+       return root->val;
+    
+    //Right subtree
+    return solve(root->right, i, k);
+}
+
+    int kthSmallest(TreeNode* root, int k) {
+    int i=0;
+    int ans = solve(root, i, k);
+    return ans;
+    }
+int main() {
+  std::cout << "Hello World!\n";
+}
