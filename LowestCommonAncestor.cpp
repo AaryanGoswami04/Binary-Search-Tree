b5064b0328c70a533758986ab5f4b2a9da348996
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-13 09:24:52 +0530

    Update and rename main (11).cpp to LowestCommonAncestor.cpp

--- LowestCommonAncestor.cpp
+++ LowestCommonAncestor.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 //Iterative approach: Space complexity:O(1)
+//Recursive approach takes O(N) Space
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
     TreeNode* temp = root;
     while(temp != NULL){
         if(temp->val < p->val && temp->val < q->val)
               temp = temp->right;
-
         else if(temp->val > p->val && temp->val > q->val)
                temp = temp->left;
         else 
@@ -14,6 +14,3 @@
     return temp;
     }
 };
-int main() {
-  std::cout << "Hello World!\n";
-}
