fa1c8fc92658f2e95dc12e45c74f2ec4a3f0987c
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-17 10:04:11 +0530

    Add files via upload

---
+++ main (11).cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+class Solution {
+    //Time complexity:O(N), Space complexity:O(N)
+private:
+    TreeNode *first;
+    TreeNode *prev;
+    TreeNode *middle;
+    TreeNode *last;
+public:
+    void inorder(TreeNode* root){
+        if(!root)
+            return;
+        inorder(root->left);
+
+        if(root->val < prev->val){
+            //If this is first violation
+            //mark these nodes first and middle
+            if(first == NULL){
+                first = prev;
+                middle = root;
+            }
+            else   //if this is second violation, mark last as root
+                last = root;
+        }
+        prev = root;
+        inorder(root->right);
+    }
+    void recoverTree(TreeNode* root) {
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+
+        inorder(root);
+
+        if(first && last)
+            swap(first->val, last->val);
+        else
+            swap(first->val, middle->val);
+    }
+};
+//https://www.youtube.com/watch?v=ZWGW7FminDM&t=819s
+int main() {
+  std::cout << "Hello World!\n";
+}
